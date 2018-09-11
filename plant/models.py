417f101d7b9b7365309fb18484f944b6d5cb46cb
Gurjeet Singh <developer.gurjeet@gmail.com>
2018-09-11 22:19:25 +0530

    created plant app

--- plant/models.py
+++ plant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    plant_id = models.CharField(max_length=100)
+    plant_name = models.CharField(max_length=500)
+    plant_detail = models.TextField(blank=True, null=True)
+
+
+class Product(models.Model):
+    plant_id = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    prooduct_family = models.CharField(max_length=500)
