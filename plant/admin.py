be178c4b78f23fcba2a1b2c1ec841b535a1d58d0
Gurjeet Singh <developer.gurjeet@gmail.com>
2018-09-11 23:26:21 +0530

    test

--- plant/admin.py
+++ plant/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Plant, Product
 
+admin.site.site_header = 'DLP administration'
+
 
 class PlantViewAdmin(admin.ModelAdmin):
     list_display = ['plant_id', 'plant_name']
