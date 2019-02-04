54f564aca4d4e9f3ef06abf46ab127cce83520fd
Gurjeet singh <developer.gurjeet@gmail.com>
2019-02-04 07:17:29 -0500

    Added code

--- plant/admin.py
+++ plant/admin.py
@@ -16,15 +16,26 @@ class ProductViewAdmin(admin.ModelAdmin):
 
 
 class PlanningDataViewAdmin(admin.ModelAdmin):
-    list_display = ['plant_id', 'FG_code', 'DT_hrs']
+    list_display = ['plant_id', 'fg_description', 'FG_code', 'DT_hrs', 'month', 'yearly_product_quantity', 'dt_value']
+
+    def plant_id(self, instance):
+        return instance.product_id.plant_id
+
+    def fg_description(self, instance):
+        return instance.product_id
 
 
 class HeadCountViewAdmin(admin.ModelAdmin):
-    list_display = ['ker', 'no_of_working_days', 'working_hours_per_day', 'testing_people']
+    list_display = ['plant_id', 'plant_family', 'month', 'ker', 'quantity']
+
+    def plant_id(self, instance):
+        return instance.product_id.plant_id
+
+    def plant_family(self, instance):
+        return instance.product_id
 
 
 admin.site.register(Plant, PlantViewAdmin)
 admin.site.register(Product)
-admin.site.register(PlanningData)
-admin.site.register(HeadCount)
-
+admin.site.register(PlanningData, PlanningDataViewAdmin)
+admin.site.register(HeadCount, HeadCountViewAdmin)
--- plant/models.py
+++ plant/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.db.models import Sum
+
+MONTH_CHOICES = (
+    ('1', 'January'),
+    ('2', 'February'),
+    ('3', 'March'),
+    ('4', 'April'),
+    ('5', 'May'),
+    ('6', 'June'),
+    ('7', 'July'),
+    ('8', 'August'),
+    ('9', 'September'),
+    ('10', 'October'),
+    ('11', 'November'),
+    ('12', 'December'),
+)
 
 
 class Plant(models.Model):
@@ -19,21 +35,39 @@ class Product(models.Model):
 
 
 class PlanningData(models.Model):
+    class Meta:
+        verbose_name_plural = 'Planning Data'
+
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     FG_code = models.CharField(max_length=500)
-    DT_hrs = models.DecimalField(max_digits=5, decimal_places=2)
+    DT_hrs = models.DecimalField(max_digits=20, decimal_places=2)
+    month = models.CharField(max_length=2, choices=MONTH_CHOICES)
+    yearly_product_quantity = models.DecimalField(max_digits=20, decimal_places=2)
+
+
+    @property
+    def dt_value(self):
+        return (self.DT_hrs * self.yearly_product_quantity)
+
+    def __str__(self):
+        return "{} | {}".format(self.product_id, self.FG_code)
 
 
 class HeadCount(models.Model):
-    ker = models.DecimalField(max_digits=5, decimal_places=2)
-    no_of_working_days = models.DecimalField(max_digits=5, decimal_places=2)
-    working_hours_per_day = models.DecimalField(max_digits=5, decimal_places=2)
-    testing_people = models.DecimalField(max_digits=5, decimal_places=2)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    month = models.CharField(max_length=2, choices=MONTH_CHOICES)
+    ker = models.DecimalField(max_digits=15, decimal_places=2)
+    no_of_working_days = models.DecimalField(max_digits=15, decimal_places=2)
+    working_hours_per_day = models.DecimalField(max_digits=15, decimal_places=2)
+    testing_people = models.DecimalField(max_digits=15, decimal_places=2)
+
+    @property
+    def quantity(self):
+        qty = PlanningData.objects.filter(month=self.month).aggregate(Sum('yearly_product_quantity'))
+        return qty['yearly_product_quantity__sum']
 
 
-
-
-
-
+    def __str__(self):
+        return self.month
 
 
